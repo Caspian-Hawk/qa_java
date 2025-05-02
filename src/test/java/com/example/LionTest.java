c8f327a4996775e3264015492d8048d5d76492fc
Caspian-Hawk <roman.kreiser.1990@gmail.com>
2025-05-02 21:25:02 +1000

    Привет, Алеся. Я почистил ветку main. И добавил параметризацию в LionTest. Добавил в pom.xml явную кодировку.

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -3,59 +3,62 @@ package com.example;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
-
+import org.mockito.MockitoAnnotations;
 import java.util.List;
-
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(Parameterized.class)
 public class LionTest {
 
     @Mock
     private Feline feline;
 
-    private Lion maleLion;
-    private Lion femaleLion;
+    private Lion lion;
+    private final String sex;
+    private final boolean hasMane;
+
+    public LionTest(String sex, boolean hasMane) {
+        this.sex = sex;
+        this.hasMane = hasMane;
+    }
+
+    @Parameterized.Parameters(name = "ExpectedSex = {0}, hasMane = {1}")
+    public static Object[][] dataSex() {
+        return new Object[][]{
+                {"Самец", true},
+                {"Самка", false},
+        };
+    }
 
     @Before
     public void setUp() throws Exception {
-        maleLion = new Lion("Самец", feline);
-        femaleLion = new Lion("Самка", feline);
+        MockitoAnnotations.openMocks(this);
+        lion = new Lion(sex, feline);
     }
 
     @Test
-    public void checkGetKittensFemaleLion() {
-        when(feline.getKittens()).thenReturn(1);
-        assertEquals(1, femaleLion.getKittens());
-    }
-
-    @Test
-    public void checkGetKittensMaleLion() {
-        when(feline.getKittens()).thenReturn(1);
-        assertEquals(1, maleLion.getKittens());
-    }
-
-    @Test
-    public void checkDoesHaveManeMale() {
-        assertTrue(maleLion.doesHaveMane());
-    }
-
-    @Test
-    public void checkDoesHaveManeFemale() {
-        assertFalse(femaleLion.doesHaveMane());
+    public void checkLionMane() throws Exception {
+        lion = new Lion(sex, feline);
+        assertEquals(hasMane, lion.doesHaveMane());
     }
 
     @Test(expected = Exception.class)
-    public void checkExceptionMane() throws Exception {
+    public void checkInvalidSex() throws Exception {
         new Lion("Оно", feline);
+    }
+
+    @Test
+    public void checkGetKittens() throws Exception {
+        when(feline.getKittens()).thenReturn(1);
+        assertEquals(1, lion.getKittens());
     }
 
     @Test
     public void checkGetFood() throws Exception {
         when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
-        assertEquals(List.of("Животные", "Птицы", "Рыба"), maleLion.getFood());
+        assertEquals(List.of("Животные", "Птицы", "Рыба"), lion.getFood());
     }
 }
